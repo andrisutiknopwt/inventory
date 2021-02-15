6964a7f89a2bb97846c797e3f2ef3b4183456fa1
fathurohman403 <fathuonline@gmail.com>
2021-02-15 14:38:46 +0700

    migrate

---
+++ app/Database/Migrations/2021-02-05-021254_user_level.php
@@ -0,0 +1,35 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UserLevel extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id_level'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => true,
+				'auto_increment' => true,
+			],
+			'level_name'       => [
+				'type'       => 'VARCHAR',
+				'constraint' => '50',
+			],
+			'status'       => [
+				'type'       => 'INT',
+				'constraint' => '1',
+			],
+		]);
+		$this->forge->addKey('id', true);
+		$this->forge->createTable('user_level');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('user_level');
+	}
+}
